db0a8e4fa72101c11a2fa9dc3f23cec294aee456
shiam <shiamabdullah1@gmail.com>
2021-10-04 14:03:02 +0600

    Category Done

--- ecom/api/urls.py
+++ ecom/api/urls.py
@@ -3,6 +3,7 @@ from rest_framework.authtoken import views
 from api.views import home
 urlpatterns = [
     path('', home, name='api.home'),
+    path('category/', include('api.category.urls')),
     
 
 ]
