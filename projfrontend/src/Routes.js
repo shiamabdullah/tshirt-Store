3b12e5c4893d897a8ecf65be4e47dd1643464b97
shiam <shiamabdullah1@gmail.com>
2021-10-15 17:37:46 +0600

    signup route

--- projfrontend/src/Routes.js
+++ projfrontend/src/Routes.js
@@ -1,7 +1,8 @@
 import React from "react";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
-import PrivateRoutes from "./auth/helper/PrivateRoutes";
 import Home from "./core/Home";
+import Signup from "./user/Signup";
+import PrivateRoutes from "./auth/helper/PrivateRoutes";
 
 const Routes = () => {
   return (
@@ -9,6 +10,7 @@ const Routes = () => {
       <Router>
         <Switch>
           <Route path="/" exact component={Home} />
+          <Route path="/signup" exact component={Signup} />
           {/* <PrivateRoutes path="/user/dashboard" exact component={} /> */}
         </Switch>
       </Router>
---
+++ projfrontend/src/user/Signup.js
@@ -0,0 +1,12 @@
+import React from "react";
+import Base from "../core/Base";
+
+const Signup = () => {
+  return (
+    <Base title="Sign Up Page" description="Sign up Tshirtee user">
+      <p>Test of signup Page</p>
+    </Base>
+  );
+};
+
+export default Signup;
