7b08d19813dfc131344fc2f5650f9d37b6e495ca
shiam <shiamabdullah1@gmail.com>
2021-10-21 16:33:10 +0600

    client token

--- projfrontend/src/core/PaymentB.js
+++ projfrontend/src/core/PaymentB.js
@@ -1,1 +1,46 @@
+import React, { useState, useEffect } from "react";
+import { Redirect } from "react-router-dom";
+import { cartEmpty } from "./helper/cartHelper";
+import { getMeToken, processPayment } from "./helper/paymentHelper";
+import { createOrder } from "./helper/orderHelper";
+import { isAuthenticated, signout } from "../auth/helper";
+import DropIn from "braintree-web-drop-in-react";
 //payment
+
+const PaymentB = ({ products, reload = undefined, setReload = (f) => f }) => {
+  const [info, setInfo] = useState({
+    loading: false,
+    success: false,
+    clientToken: null,
+    error: false,
+    instance: {}, //onInstance
+  });
+
+  const userId = isAuthenticated && isAuthenticated().user.id;
+  const token = isAuthenticated && isAuthenticated().token;
+
+  const getToken = (userId, token) => {
+    getMeToken(userId, token).then((info) => {
+      if (info.error) {
+        setInfo({
+          ...info,
+          error: info.error,
+        });
+        signout(() => {
+          return <Redirect to="/"></Redirect>;
+        });
+      } else {
+        const clientToken = info.clientToken;
+        setInfo({ clientToken });
+      }
+    });
+  };
+
+  return (
+    <div>
+      <h1>Payment B</h1>
+    </div>
+  );
+};
+
+export default PaymentB;
